5e0517060b19961637f705a2d684d7050e224117
Jens Lundberg <jens.lundberg90@gmail.com>
2019-04-23 15:10:32 +0200

    Add survey repository and some changes to views

---
+++ src/main/java/com/considLia/survey/repositories/SurveyRepository.java
@@ -0,0 +1,10 @@
+package com.considLia.survey.repositories;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.considLia.survey.model.Survey;
+
+public interface SurveyRepository extends JpaRepository<Survey, Long> {
+  Survey findByUsername(String username);
+
+}
--- src/main/java/com/considLia/survey/ui/ShowSurveyView.java
+++ src/main/java/com/considLia/survey/ui/ShowSurveyView.java
@@ -3,7 +3,7 @@ package com.considLia.survey.ui;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 
-@Route(value = "", layout = MainLayout.class)
+@Route(value = "showsurvey", layout = MainLayout.class)
 public class ShowSurveyView extends VerticalLayout {
 
   public ShowSurveyView() {
