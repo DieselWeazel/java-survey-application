f510873de96e0bad12eaa8b2f9c75f07263010b1
August Filipsson <august.filipsson@gmail.com>
2019-04-23 13:32:07 +0000

    Update SurveyRepository.java

--- src/main/java/com/considLia/survey/repositories/SurveyRepository.java
+++ src/main/java/com/considLia/survey/repositories/SurveyRepository.java
@@ -5,6 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.considLia.survey.model.Survey;
 
 public interface SurveyRepository extends JpaRepository<Survey, Long> {
-  Survey findByUsername(String username);
-
 }
