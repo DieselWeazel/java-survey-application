b05316953799a1fa0943ecb71b6b179dd1e024e7
Jens Lundberg <jens.lundberg90@gmail.com>
2019-04-23 14:17:18 +0200

    Add two more views and set them up

---
+++ src/main/java/com/considLia/survey/ui/MainLayout.java
@@ -0,0 +1,20 @@
+package com.considLia.survey.ui;
+
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.AfterNavigationEvent;
+import com.vaadin.flow.router.AfterNavigationObserver;
+import com.vaadin.flow.router.RouterLayout;
+
+public class MainLayout extends VerticalLayout implements RouterLayout, AfterNavigationObserver {
+
+  public MainLayout() {
+
+  }
+
+  @Override
+  public void afterNavigation(AfterNavigationEvent event) {
+    // TODO Auto-generated method stub
+
+  }
+
+}
