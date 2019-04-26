1edf2737f9282de6570404f20d5c50d835df5ebb
Sara Fällström <s.e.fallstrom@gmail.com>
2019-04-26 11:27:23 +0200

    Snyggat till koden

--- src/main/java/com/considLia/survey/ui/MainView.java
+++ src/main/java/com/considLia/survey/ui/MainView.java
@@ -13,88 +13,22 @@ public class MainView extends VerticalLayout {
 
   public MainView(SurveyRepository surveyRepository) {
 
-
-
     List<Survey> surveyList = new ArrayList<>();
 
     // Hämtar lista från Databasen
     surveyList = surveyRepository.findAll();
 
+    Grid<Survey> grid = new Grid<>();
+    Grid.Column<Survey> idColumn = grid.addColumn(Survey::getSurveyId).setHeader("Id");
+    Grid.Column<Survey> titleColumn = grid.addColumn(Survey::getSurveyTitle).setHeader("Title");
+    Grid.Column<Survey> creatorColumn = grid.addColumn(Survey::getCreator).setHeader("Creator");
+    Grid.Column<Survey> dateColumng = grid.addColumn(Survey::getDate).setHeader("Date");
 
-    if (!surveyList.isEmpty()) {
-      Grid<Survey> grid = new Grid<>();
-      Grid.Column<Survey> idColumn = grid.addColumn(Survey::getSurveyId).setHeader("Id");
-      Grid.Column<Survey> titleColumn = grid.addColumn(Survey::getSurveyTitle).setHeader("Title");
-      Grid.Column<Survey> creatorColumn = grid.addColumn(Survey::getCreator).setHeader("Creator");
-      Grid.Column<Survey> dateColumng = grid.addColumn(Survey::getDate).setHeader("Date");
-      grid.setItems(surveyList);
-      add(grid);
-
-    }
-
-    else {
-      Grid<Survey> grid = new Grid<>();
+    if (surveyList.isEmpty()) {
       grid.setHeight("80px");
-      Grid.Column<Survey> idColumn = grid.addColumn(Survey::getSurveyId).setHeader("Id");
-      Grid.Column<Survey> titleColumn = grid.addColumn(Survey::getSurveyTitle).setHeader("Title");
-      Grid.Column<Survey> creatorColumn = grid.addColumn(Survey::getCreator).setHeader("Creator");
-      Grid.Column<Survey> dateColumng = grid.addColumn(Survey::getDate).setHeader("Date");
-      add(grid);
-
     }
-    // TextField id = new TextField("ID");
-    // TextField title = new TextField("Survey title");
-    // TextField creator = new TextField("Survey creator");
-    // TextField date = new TextField("Date");
-    //
-    // FormLayout form = new FormLayout(id, title, creator, date);
-    //
-    //
-    //
-    // add(form);
+    grid.setItems(surveyList);
 
-
-
-    // Binder<Person> binder = new Binder<>(Person.class);
-    // binder.bind(firstName, Person::getFirstName, Person::setFirstName);
-    // binder.bind(lastName, Person::getLastName, Person::setLastName);
-
-    // return new BinderCrudEditor<>(binder, form);
-
-    // Crud<Survey> crud = new Crud<>(Survey.class, createPersonEditor());
-    //
-    // PersonDataProvider dataProvider = new PersonDataProvider();
-    //
-    // crud.getGrid().removeColumnByKey("id");
-    // crud.setDataProvider(dataProvider);
-    // crud.addSaveListener(e -> dataProvider.persist(e.getItem()));
-    // crud.addDeleteListener(e -> dataProvider.delete(e.getItem()));
-    //
-    // crud.addThemeVariants(CrudVariant.NO_BORDER);
-
-    // private CrudEditor<Person> createPersonEditor() {
-
-
-
-    // }
-
-
-
-    // Survey survey = surveyRepository.getSurveyBySurveyId(1l);
-    //
-    // add(new H1(survey.getSurveyTitle() + " " + survey.getCreator() + " " + survey.getDate()));
-    //
-    // survey.getQuestionList();
-    //
-    // for (Question q : survey.getQuestionList()) {
-    // add(new H2(q.getQuestionTitle()));
-    // if (q.getAlternativeList() != null) {
-    // for (MultiQuestionAlternative mqa : q.getAlternativeList()) {
-    // add(new H3(mqa.getAlternativeTitle()));
-    // }
-    // }
-    // }
-
+    add(grid);
   }
-
 }
