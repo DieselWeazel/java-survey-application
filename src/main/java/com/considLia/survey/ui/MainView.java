7905234e9a49553f67822be5bc3f762f47a2d7dd
Jonathan Harr <jonathan.harr92@gmail.com>
2019-04-26 09:06:08 +0200

    Began work on Binder.

--- src/main/java/com/considLia/survey/ui/MainView.java
+++ src/main/java/com/considLia/survey/ui/MainView.java
@@ -4,17 +4,38 @@ import com.considLia.survey.model.MultiQuestionAlternative;
 import com.considLia.survey.model.Question;
 import com.considLia.survey.model.Survey;
 import com.considLia.survey.repositories.SurveyRepository;
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.html.Anchor;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.html.H3;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 
+import java.util.List;
+
 @Route(value = "", layout = MainLayout.class)
 public class MainView extends VerticalLayout {
 
-  public MainView(SurveyRepository surveyRepository) {
+  // -- Only For Use With Testing, Delete when Merging --
+  private Grid<Survey> grid = new Grid<>(Survey.class);
 
+  public MainView(SurveyRepository surveyRepository) {
+    // -- Only For Use With Testing, Delete when Merging --
+    add(new H1("Survey List"));
+    List<Survey> surveyList = surveyRepository.findAll();
+
+    grid.setColumns("surveyTitle", "creator", "date");
+    grid.setItems(surveyList);
+    grid.addComponentColumn(survey -> {
+      String route = UI.getCurrent().getRouter()
+              .getUrl(ShowSurveyView.class, survey.getSurveyId());
+      return new Anchor(route, "Details");
+    });
+    // -- Only For Use With Testing, Delete when Merging --
+
+    add(grid);
     Survey survey = surveyRepository.getSurveyBySurveyId(1l);
 
     add(new H1(survey.getSurveyTitle() + " " + survey.getCreator() + " " + survey.getDate()));
--- src/main/java/com/considLia/survey/ui/ShowSurveyView.java
+++ src/main/java/com/considLia/survey/ui/ShowSurveyView.java
@@ -1,12 +1,86 @@
 package com.considLia.survey.ui;
 
+import com.considLia.survey.model.MultiQuestionAlternative;
+import com.considLia.survey.model.Question;
+import com.considLia.survey.model.Survey;
+import com.considLia.survey.repositories.SurveyRepository;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.H3;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.binder.Binder;
+import com.vaadin.flow.router.BeforeEvent;
+import com.vaadin.flow.router.HasUrlParameter;
 import com.vaadin.flow.router.Route;
 
-@Route(value = "showsurvey", layout = MainLayout.class)
-public class ShowSurveyView extends VerticalLayout {
+import javax.validation.constraints.Null;
 
-  public ShowSurveyView() {
+@Route(value = "showsurvey", layout = MainLayout.class)
+public class ShowSurveyView extends VerticalLayout implements HasUrlParameter<Long> {
+
+  // -- Private Variables --
+  private SurveyRepository surveyRepository;
+
+  // -- TextContainers --
+  private H1 h1;
+
+  // -- Layout --
+  public ShowSurveyView(SurveyRepository surveyRepository) {
+    this.surveyRepository = surveyRepository;
+    h1 = new H1("lol");
+
+    add(h1);
+  }
+
+  // -- Parameter Method, finding Survey --
+  @Override
+  public void setParameter(BeforeEvent event, Long parameter) {
+    try {
+      if (parameter == null) {
+        // Null Pointer Exception? (Survey Null Pointer Exception)
+        h1.setText("ERROR, no survey ID parameter given.");
+      }
+      if (surveyRepository.findById(parameter).isPresent()) {
+        Survey survey = surveyRepository.getSurveyBySurveyId(parameter);
+        h1.setText(survey.getSurveyTitle());
+      loadSurvey(survey);
+      } else {
+        // Or Create SurveyNotFoundException
+        h1.setText("ERROR, no Survey by the ID of: " + parameter + " exists.");
+      }
+    } catch (NullPointerException e){
+      h1.setText(e.getMessage());
+    }
+  }
+
+  // -- Loading Survey to Layout
+  public void loadSurvey(Survey survey) {
+//    for (Question q : survey.getQuestionList()) {
+//      add(new H1(q.getQuestionTitle()));
+//      add(new TextField());
+//      if (q.getAlternativeList() != null) {
+//        add(new H1(q.getQuestionTitle()));
+//
+//        for (MultiQuestionAlternative mqa : q.getAlternativeList()) {
+//          add(new H1(mqa.getAlternativeTitle()));
+//        }
+//      }
+//    }
+
+    // java.lang.IllegalArgumentException: Could not resolve property name Test textfråga from Property set for bean com.considLia.survey.model.Survey
+    // Bean issue?
+    Binder<Survey> binder = new Binder<>(Survey.class);
+    TextField textField = new TextField();
+
+    for (Question q: survey.getQuestionList()){
+      binder.forField(textField).bind(q.getQuestionTitle());
+    }
+
+    binder.setBean(survey);
+
+
+
 
   }
 }
+
