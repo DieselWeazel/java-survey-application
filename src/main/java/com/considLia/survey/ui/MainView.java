f57253d29361b4ce3d838c06061a610296181a54
Andreas Hellström <andreas.hellstrom@yaelev.se>
2019-04-27 21:01:58 +0200

    Added component column to grid that show three buttons(show, edit and delete). Only delete button has event listener for now.
    Custom confirm dialog added(is called on when deleteBtn is pressed).

--- src/main/java/com/considLia/survey/ui/MainView.java
+++ src/main/java/com/considLia/survey/ui/MainView.java
@@ -2,16 +2,24 @@ package com.considLia.survey.ui;
 
 import java.util.ArrayList;
 import java.util.List;
+import com.considLia.survey.custom_component.ConfirmDialog;
 import com.considLia.survey.model.Survey;
 import com.considLia.survey.repositories.SurveyRepository;
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 
 @Route(value = "", layout = MainLayout.class)
 public class MainView extends VerticalLayout {
 
+  private SurveyRepository surveyRepository;
+
   public MainView(SurveyRepository surveyRepository) {
+    this.surveyRepository = surveyRepository;
 
     List<Survey> surveyList = new ArrayList<>();
 
@@ -19,10 +27,11 @@ public class MainView extends VerticalLayout {
     surveyList = surveyRepository.findAll();
 
     Grid<Survey> grid = new Grid<>();
-    Grid.Column<Survey> idColumn = grid.addColumn(Survey::getSurveyId).setHeader("Id");
-    Grid.Column<Survey> titleColumn = grid.addColumn(Survey::getSurveyTitle).setHeader("Title");
-    Grid.Column<Survey> creatorColumn = grid.addColumn(Survey::getCreator).setHeader("Creator");
-    Grid.Column<Survey> dateColumng = grid.addColumn(Survey::getDate).setHeader("Date");
+    grid.addColumn(Survey::getSurveyId).setHeader("Id");
+    grid.addColumn(Survey::getSurveyTitle).setHeader("Title");
+    grid.addColumn(Survey::getCreator).setHeader("Creator");
+    grid.addColumn(Survey::getDate).setHeader("Date");
+    grid.addComponentColumn(item -> showButtons(grid, item));
 
     if (surveyList.isEmpty()) {
       grid.setHeight("80px");
@@ -30,5 +39,16 @@ public class MainView extends VerticalLayout {
     grid.setItems(surveyList);
 
     add(grid);
+  }
+
+  public HorizontalLayout showButtons(Grid<Survey> grid, Survey item) {
+    Button showSurvey = new Button(new Icon(VaadinIcon.EYE));
+    Button editSurvey = new Button(new Icon(VaadinIcon.PENCIL));
+    Button deleteSurvey = new Button(new Icon(VaadinIcon.TRASH), onDelete -> {
+      ConfirmDialog confirmDialog = new ConfirmDialog("Confirm delete",
+          "Are you sure you want to delete the item?", surveyRepository, grid, item);
+    });
+
+    return new HorizontalLayout(showSurvey, editSurvey, deleteSurvey);
   }
 }
