d80ef30bbb1e9c4c2d0aff2dcdab5be96ccb0ba0
Jens Lundberg <jens.lundberg90@gmail.com>
2019-05-13 09:12:25 +0200

    Add baseclass for read components

---
+++ src/main/java/com/considlia/survey/ui/custom_component/ReadQuestionLayout.java
@@ -0,0 +1,17 @@
+package com.considlia.survey.ui.custom_component;
+
+import com.considlia.survey.model.Question;
+import com.vaadin.flow.component.html.H5;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+
+public abstract class ReadQuestionLayout extends VerticalLayout {
+
+  private H5 title;
+
+  public ReadQuestionLayout(Question question) {
+    this.title = new H5(question.getTitle() + (question.isMandatory() ? "*" : ""));
+
+    add(title);
+  }
+
+}
