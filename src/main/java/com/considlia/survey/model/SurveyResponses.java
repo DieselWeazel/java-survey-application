17037e1116b9f6ab43bd0f1db62dd4008d314cf1
Jens Lundberg <jens.lundberg90@gmail.com>
2019-05-14 11:27:13 +0200

    Add mock class for Answers and some additions to SurveyResponses

--- src/main/java/com/considlia/survey/model/SurveyResponses.java
+++ src/main/java/com/considlia/survey/model/SurveyResponses.java
@@ -1,5 +1,73 @@
 package com.considlia.survey.model;
 
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OrderBy;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "surveyresponses")
 public class SurveyResponses {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "response_id")
+  private Long id;
+  private Long surveyId;
+  private LocalDate date = LocalDate.now();
+
+  @OneToMany(orphanRemoval = true, cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+  @JoinColumn(name = "response_id")
+  @OrderBy("position ASC")
+  private Set<Answers> answers = new HashSet<>();
+
+  public SurveyResponses() {}
+
+  public SurveyResponses(Long surveyId, LocalDate date, Set<Answers> answers) {
+    this.surveyId = surveyId;
+    this.date = date;
+    this.answers = answers;
+  }
+
+  public void addAnswer(Answers answer) {
+    getAnswers().add(answer);
+  }
+
+  public Long getSurveyId() {
+    return surveyId;
+  }
+
+  public void setSurveyId(Long surveyId) {
+    this.surveyId = surveyId;
+  }
+
+  public LocalDate getDate() {
+    return date;
+  }
+
+  public void setDate(LocalDate date) {
+    this.date = date;
+  }
+
+  public Set<Answers> getAnswers() {
+    return answers;
+  }
+
+  public void setAnswers(Set<Answers> answers) {
+    this.answers = answers;
+  }
+
+  public Long getId() {
+    return id;
+  }
 }
