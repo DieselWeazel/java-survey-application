ecaa4abf870696b4593262e590625a5331684990
Jonathan Harr <jonathan.harr92@gmail.com>
2019-05-16 10:21:43 +0200

    Implemented a CustomUserService to get the User out of a Service instead of repeating code.

--- src/main/java/com/considlia/survey/security/CustomUserService.java
+++ src/main/java/com/considlia/survey/security/CustomUserService.java
@@ -1,12 +1,34 @@
 package com.considlia.survey.security;
 
 import com.considlia.survey.model.User;
+import com.considlia.survey.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CustomUserService extends User {
 
+  @Autowired
+  private UserRepository userRepository;
 
+  public CustomUserService(UserRepository userRepository) {
+    this.userRepository = userRepository;
+  }
 
+  public User getUser(){
+    Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    User user = new User();
+    // If Principal is of type User, get the User.
+    // Mandatory
+    if (principal instanceof UserDetails) {
+      String username = ((UserDetails) principal).getUsername();
 
+      user = userRepository.findByUsername(username);
+
+      return user;
+    }
+    return user;
+  }
 }
