c9715ce97baf4aeafb2344977acc442e9a368cba
Jonathan Harr <jonathan.harr92@gmail.com>
2019-05-22 15:40:14 +0200

    Added CheckBoxQuestion properly.

---
+++ src/main/java/com/considlia/survey/model/question/CheckBoxQuestion.java
@@ -0,0 +1,21 @@
+package com.considlia.survey.model.question;
+
+import com.considlia.survey.model.QuestionType;
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "checkboxquestion")
+public class CheckBoxQuestion extends MultiQuestion {
+
+  public CheckBoxQuestion() {
+
+  }
+
+  public CheckBoxQuestion(String questionTitle, boolean mandatory,
+      List<String> stringAlternatives) {
+    super(questionTitle, QuestionType.CHECKBOX, mandatory, stringAlternatives);
+  }
+
+}
