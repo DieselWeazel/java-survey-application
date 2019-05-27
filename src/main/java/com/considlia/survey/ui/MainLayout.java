485ad3727d9f93badaf50c35c8945fc82f1752e9
August <august.filipsson@gmail.com>
2019-05-27 11:04:50 +0200

    Added Javadoc

--- src/main/java/com/considlia/survey/ui/MainLayout.java
+++ src/main/java/com/considlia/survey/ui/MainLayout.java
@@ -1,5 +1,10 @@
 package com.considlia.survey.ui;
 
+import java.util.HashMap;
+import java.util.Map;
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import com.considlia.survey.security.SecurityUtils;
 import com.considlia.survey.ui.UserViews.AccessDeniedView;
 import com.considlia.survey.ui.UserViews.LoginView;
@@ -16,11 +21,6 @@ import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.RouterLayout;
 import com.vaadin.flow.router.RouterLink;
-import java.util.HashMap;
-import java.util.Map;
-import javax.servlet.http.HttpServletRequest;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.context.SecurityContextHolder;
 
 @StyleSheet("css/app.css")
 public class MainLayout extends VerticalLayout implements RouterLayout {
@@ -33,6 +33,9 @@ public class MainLayout extends VerticalLayout implements RouterLayout {
   @Autowired
   private HttpServletRequest httpServletRequest;
 
+  /**
+   * Manages header for site. Adds different buttons depending on if user is logged in.
+   */
   public MainLayout() {
     setId("mainlayout");
 
@@ -56,6 +59,9 @@ public class MainLayout extends VerticalLayout implements RouterLayout {
     add(navigation, contentContainer);
   }
 
+  /**
+   * Logs the user out. Reloads the page.
+   */
   private void logoutUser() {
     SecurityContextHolder.clearContext();
     httpServletRequest.getSession(false).invalidate();
@@ -63,8 +69,17 @@ public class MainLayout extends VerticalLayout implements RouterLayout {
     UI.getCurrent().getPage().reload();
   }
 
-  private Button createRouterLink(
-      Class<? extends Component> targetViewClass, String text, VaadinIcon icon) {
+  /**
+   * Creates a {@link Button} with a link to a new view. Checks {@link SecurityUtils} before sending
+   * user to targetViewClass
+   * 
+   * @param targetViewClass, the class the Button should navigate to
+   * @param text, text shown to user on Button
+   * @param icon
+   * @returns new {@link Button}
+   */
+  private Button createRouterLink(Class<? extends Component> targetViewClass, String text,
+      VaadinIcon icon) {
     return new Button(text, new Icon(icon), e -> {
       if (SecurityUtils.hasAccess(targetViewClass)) {
         getUI().ifPresent(ui -> ui.navigate(targetViewClass));
@@ -74,6 +89,17 @@ public class MainLayout extends VerticalLayout implements RouterLayout {
     });
   }
 
+  /**
+   * Shows the content of the layout which is the router target component annotated with a
+   * {@link @Route}.
+   * 
+   * Note implementors should not care about old Route content, because Router automatically removes
+   * it before calling the method.
+   * 
+   * Specified by: showRouterLayoutContent(...) in RouterLayout
+   * 
+   * @param: content the content component or null if the layout content is to be cleared.
+   */
   @Override
   public void showRouterLayoutContent(HasElement content) {
     if (content != null) {
